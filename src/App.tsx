562d559aa750a7f02b6eac92e64810d4989257b4
Kaio Angelo <kposansky@gmail.com>
2025-10-25 20:39:29 -0300

    feat: toasts na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { AuthProvider } from "./context/AuthContext";
 import { toastConfig } from "./config/toast.config";
+import Toast from 'react-native-toast-message';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -22,6 +23,7 @@ export default function App() {
         <ScreenContent title="Home" path="src/App.tsx"></ScreenContent>
         <StatusBar style="auto" />
       </Providers>
+      <Toast config={toastConfig}/>
     </>
   );
 }
