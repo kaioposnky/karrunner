cecb601d1ab2accfa33723ccfbf9ad01a4cb7af0
Kaio Angelo <kposansky@gmail.com>
2025-10-25 18:11:11 -0300

    refactor: adicionado centralizacao a um so view

---
+++ src/components/themed/ThemedView.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react';
+import { View, Text, ViewProps } from 'react-native';
+import { themeConfig } from '@/config/theme.config';
+import { useTheme } from '@/hooks/useTheme';
+
+interface IThemeViewCenterHProps extends ViewProps{
+  center?: "horizontal" | "vertical" | "both";
+  className?: string;
+}
+
+export const ThemedView: FC<IThemeViewCenterHProps> = ({
+  center, children, className='', ...props
+                                                       }) => {
+  const {theme} = useTheme();
+
+  const viewColor = themeConfig.view.background[theme];
+  let centerClasses = '';
+  if(center){
+    if(center === "horizontal"){
+      centerClasses = 'items-center';
+    } else if(center === "vertical"){
+      centerClasses = 'justify-center';
+    } else if (center === "both"){
+      centerClasses = 'justify-center items-center';
+    }
+    centerClasses = `flex ${centerClasses}`;
+  }
+
+  return (
+    <View
+      className={`${className} ${viewColor} ${centerClasses}`}
+      {...props}
+    >
+      {children}
+    </View>
+  );
+};
