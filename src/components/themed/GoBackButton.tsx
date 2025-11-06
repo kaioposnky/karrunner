438e4f307ea2d924724ed3d62e6da3df589681d2
Kaio Angelo <kposansky@gmail.com>
2025-11-06 00:56:13 -0300

    fix: Component de botão de voltar sem prop style

--- src/components/themed/GoBackButton.tsx
+++ src/components/themed/GoBackButton.tsx
@@ -1,13 +1,14 @@
 import { useNavigation } from "@react-navigation/native";
 import { ThemedButton } from "./ThemedButton";
 
-export const GoBackButton = ({ className }: { className?: string }) => {
+export const GoBackButton = ({ className, style }: { className?: string, style?: any }) => {
   const navigation = useNavigation();
 
   return (
     <ThemedButton
       title="Voltar"
       className={`bg-red-500 ${className}`}
+      style={style}
       onPress={() => navigation.goBack()}
     />
   );
