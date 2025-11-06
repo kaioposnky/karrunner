5604ef871134d5ec281474d8f305f9d4a2aeaf6c
Kaio Angelo <kposansky@gmail.com>
2025-11-05 23:38:07 -0300

    refactor: botao de voltar com classname

--- src/components/themed/GoBackButton.tsx
+++ src/components/themed/GoBackButton.tsx
@@ -1,13 +1,13 @@
 import { useNavigation } from "@react-navigation/native";
 import { ThemedButton } from "./ThemedButton";
 
-export const GoBackButton = () => {
+export const GoBackButton = ({ className }: { className?: string }) => {
   const navigation = useNavigation();
 
   return (
     <ThemedButton
       title="Voltar"
-      className="bg-red-500"
+      className={`bg-red-500 ${className}`}
       onPress={() => navigation.goBack()}
     />
   );
