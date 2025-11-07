e69bf3d18b1d67ce6fd06012df96bddf5683ecb1
Kaio Angelo <kposansky@gmail.com>
2025-11-07 00:04:24 -0300

    feat: imagem do carro de correr

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -34,18 +34,18 @@ export async function createCars(): Promise<void> {
   const carsRef = ref(db, 'cars'); // Reference to the list of cars
 
   const carsToCreate: { name: string, rarity: Rarity, images: { select: string, run: string }}[] = [
-    { name: 'Peugeuot 206', rarity: 'common', images: { select: "https://i.postimg.cc/nLvXCJK8/peugeot-206.png", run: ""} },
-    { name: 'Fiesta', rarity: 'common', images: { select: "https://i.postimg.cc/YS1vhH6n/fiesta.png", run: ""} },
-    { name: 'HB20S', rarity: 'common', images: { select: "https://i.postimg.cc/zfnyVrCQ/hb20s.png", run: ""} },
-    { name: 'Nivus', rarity: 'common', images: { select: "https://i.postimg.cc/XvwrpbfR/nivus.png", run: ""} },
-    { name: 'Polo', rarity: 'rare', images: { select: "https://i.postimg.cc/qRSSY9Yd/polo.png", run: ""} },
-    { name: 'Fusca Vermelho', rarity: 'rare', images: { select: "https://i.postimg.cc/6Qd8yXrY/fusca-vermelho.png", run: ""} },
-    { name: 'Virtus', rarity: 'rare', images: { select: "https://i.postimg.cc/5NYNXJ1q/virtus.png", run: ""} },
-    { name: 'Fiesta Bom', rarity: 'rare', images: { select: "https://i.postimg.cc/TP8ydwDZ/fiesta-bom.png", run: ""} },
-    { name: 'HRV', rarity: 'epic', images: { select: "https://i.postimg.cc/LszJnptw/hrv.png", run: ""} },
-    { name: 'Tiggo 5', rarity: 'epic', images: { select: "https://i.postimg.cc/jjzDLb60/tiggo.png", run: ""} },
-    { name: 'Fusca Branco', rarity: 'legendary', images: { select: "https://i.postimg.cc/8Ch7jSRw/fusca-branco.png", run: ""} },
-    { name: 'Uno', rarity: 'legendary', images: { select: "https://i.postimg.cc/GtNLX7PG/uno.png", run: ""} },
+    { name: 'Peugeuot 206', rarity: 'common', images: { select: "https://i.postimg.cc/nLvXCJK8/peugeot-206.png", run: "https://i.postimg.cc/Hxdq8fTb/peugeot.png"} },
+    { name: 'Fiesta', rarity: 'common', images: { select: "https://i.postimg.cc/YS1vhH6n/fiesta.png", run: "https://i.postimg.cc/Y9M5LJt7/fiesta.png"} },
+    { name: 'HB20S', rarity: 'common', images: { select: "https://i.postimg.cc/zfnyVrCQ/hb20s.png", run: "https://i.postimg.cc/cH0ptVdn/hb20s.png"} },
+    { name: 'Nivus', rarity: 'common', images: { select: "https://i.postimg.cc/XvwrpbfR/nivus.png", run: "https://i.postimg.cc/50bhQZfL/nivus.png"} },
+    { name: 'Polo', rarity: 'rare', images: { select: "https://i.postimg.cc/qRSSY9Yd/polo.png", run: "https://i.postimg.cc/W38QGX4D/polo.png"} },
+    { name: 'Fusca Vermelho', rarity: 'rare', images: { select: "https://i.postimg.cc/6Qd8yXrY/fusca-vermelho.png", run: "https://i.postimg.cc/vBbJxpGG/fusca-vermelho.png"} },
+    { name: 'Virtus', rarity: 'rare', images: { select: "https://i.postimg.cc/5NYNXJ1q/virtus.png", run: "https://i.postimg.cc/1tBLGJz0/virtus.png"} },
+    { name: 'Fiesta Bom', rarity: 'rare', images: { select: "https://i.postimg.cc/TP8ydwDZ/fiesta-bom.png", run: "https://i.postimg.cc/PxTgvBdn/fiesta-bom.png"} },
+    { name: 'HRV', rarity: 'epic', images: { select: "https://i.postimg.cc/LszJnptw/hrv.png", run: "https://i.postimg.cc/C1S3nWw8/hrv.png"} },
+    { name: 'Tiggo 5', rarity: 'epic', images: { select: "https://i.postimg.cc/jjzDLb60/tiggo.png", run: "https://i.postimg.cc/mDj0Y8rF/tiggo.png"} },
+    { name: 'Fusca Branco', rarity: 'legendary', images: { select: "https://i.postimg.cc/8Ch7jSRw/fusca-branco.png", run: "https://i.postimg.cc/DZn94t7F/fusca-branco.png"} },
+    { name: 'Uno', rarity: 'legendary', images: { select: "https://i.postimg.cc/GtNLX7PG/uno.png", run: "https://i.postimg.cc/G2KZFqpF/uno.png"} },
   ];
 
   for (const carData of carsToCreate) {
